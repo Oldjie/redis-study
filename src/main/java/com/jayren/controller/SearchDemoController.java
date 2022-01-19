ad9865d5d2dc22366b328218b50e0d88490035ea
jayren <jayren@jidouauto.com>
2022-01-19 18:48:19 +0800

    [add]

--- src/main/java/com/jayren/controller/SearchDemoController.java
+++ src/main/java/com/jayren/controller/SearchDemoController.java
@@ -1,6 +1,7 @@
 package com.jayren.controller;
 
 import com.jayren.service.RedisService;
+import com.jayren.utils.GetPinyin;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,20 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.Set;
 
 /**
+ *  模糊搜索索引
  * @Author Jay
  * @Date 2022/1/18 15:50
  * @Version 1.0
  */
 @RestController
 @RequestMapping("/demo")
-public class DemoController {
-    @Resource
-    private RedisTemplate<Object,Object> redisTemplate;
+public class SearchDemoController {
 
     private final String KEY = "auto_complete::";
+
+    private final  int RANKING = 10;
 
     @Resource
     private RedisService redisService;
@@ -30,14 +31,16 @@ public class DemoController {
     @GetMapping("/feed/{name}")
     public String feed(@PathVariable String name){
         long l = System.currentTimeMillis();
-        for (int i = 1; i < name.length()+1; i++) {
-            redisService.zAdd(KEY + name.substring(0,i),name,l);
+
+        String pingYin = GetPinyin.getPingYin(name);
+        for (int i = 1; i < pingYin.length()+1; i++) {
+            redisService.zAdd(KEY + pingYin.substring(0,i),name,l);
         }
         return "ok";
     }
 
     @GetMapping("/hint/{keyword}")
     public Object hint(@PathVariable String keyword){
-        return redisService.reverseRange(KEY+keyword, 0, 10);
+        return redisService.reverseRange(KEY+GetPinyin.getPingYin(keyword), 0, RANKING);
     }
 }
